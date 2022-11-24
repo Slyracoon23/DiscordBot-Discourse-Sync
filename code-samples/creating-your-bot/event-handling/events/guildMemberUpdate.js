d35dddeb493bbfc3ad93625ed081a5ade01d5949
Will Jaw <willjaw@wills-mbp.lan>
2022-11-23 20:47:08 -0800

    added axios logic

--- code-samples/creating-your-bot/event-handling/events/guildMemberUpdate.js
+++ code-samples/creating-your-bot/event-handling/events/guildMemberUpdate.js
@@ -1,11 +1,13 @@
+const axios = require("axios");
 const { Api_Key, Api_Username } = require("../config.json");
+const { discord_test } = require("../groups.json");
 
 module.exports = {
 	name: "guildMemberUpdate",
 	async execute(oldMember, newMember) {
 		//console.log(oldMember, newMember);
     
-    //var defaultHost = "https://forum.citydao.io/";
+    let defaultHost = "forum.citydao.io";
     
     /* 
     / Grab object for the new role
@@ -19,7 +21,20 @@ module.exports = {
 		console.log(`Role ID: ${newRoleId}`);
 		console.log(`Role Name: ${newRoleName}`);
 
-		console.log(newMember.roles.cache);
+		//console.log(newMember.roles.cache);
+    // Check whether we added or removed roles
+    newMemberSize = newMember.roles.cache.size;
+    oldMemberSize = oldMember.roles.cache.size;
+		console.log(`new size: ${newMemberSize}`);
+		console.log(`old size: ${oldMemberSize}`);
+		// needed to +1 for oldMemberSize due to @everyone role in discord
+    if(newMemberSize > oldMemberSize+1) {
+      // add logic for added role
+      console.log("role added");
+    } else {
+      // add logic for removed role
+      console.log("role removed");
+    };
 
     /* 
     / MemberNickName = Discord nickname of the current user (string)
@@ -36,29 +51,67 @@ module.exports = {
     // Check whether discord user name exist on discourse/forum
     // API endpoint "Get a single user by username"
     // METHOD: GET
-    // https://{defaultHost}/u/{username.json}
     //
     //
+    let url = `https://${defaultHost}/u/${MemberName}.json`;
+    console.log(url);
 
+    let postUrl = `https://${defaultHost}/groups/${discord_test}/members.json`;
+
+    let data = {
+      "usernames": MemberName
+    }
+    let putData = JSON.stringify(data);
+    
+    const headers = {
+      "Content-Type": "application/json",
+      "Api-Key": Api_Key,
+      "Api-Username": Api_Username
+    };
+
+    // If the username exist it will be added to the group
+    /*axios
+      .get(url)
+      // Check username exist on discourse
+      .then(( res ) => {
+        console.log(res.status);
+        // User found
+        // Add user to the group if they were added to the role
+        if (newMemberSize >= oldMemberSize+1) { 
+          axios.put(postUrl, putData, {
+            headers: headers
+          })
+          .then(( res ) => {
+            console.log(`User added to ${newRoleName}.`, res.status);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+        } else {
+        // Remove user from the group if they were removed from the role
+          axios.delete(postUrl, {
+            data: data
+          }, {
+            headers: headers
+          })
+          .then(( res ) => {
+            console.log(`User removed from ${newRoleName}`, res.status);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error("Username does not exist on Discourse");
+      });
+      */
+    console.log(postUrl);
     // Add user to a group
     // API endpoint "Add group members"
     // METHOD: PUT
     // https://{defaultHost}/groups/{id}/members.json
     //
     //
-
-    // Check whether we added or removed roles
-    newMemberSize = newMember.roles.cache.size;
-    oldMemberSize = oldMember.roles.cache.size;
-		console.log(`new size: ${newMemberSize}`);
-		console.log(`old size: ${oldMemberSize}`);
-		// needed to +1 for oldMemberSize due to @everyone role in discord
-    if(newMemberSize >= oldMemberSize+1) {
-      // add logic for added role
-      console.log("role added");
-    } else {
-      // add logic for removed role
-      console.log("role removed");
-    };
 	},
 };
